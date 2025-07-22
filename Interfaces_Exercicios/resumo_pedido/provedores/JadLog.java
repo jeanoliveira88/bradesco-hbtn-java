474c6c6da606d133bd5a149d25abb8a36b444f34
jean.oliveira <jean.valid@gmail.com>
2025-07-22 17:36:19 -0300

    implementacao interfaces

---
+++ Interfaces_Exercicios/resumo_pedido/provedores/JadLog.java
@@ -0,0 +1,12 @@
+package provedores;
+
+public class JadLog implements ProvedorFrete {
+    public Frete calcularFrete(double peso, double valor) {
+        double valorFrete = (peso > 2000) ? valor * 0.07 : valor * 0.045;
+        return new Frete(valorFrete, TipoProvedorFrete.JADLOG);
+    }
+
+    public TipoProvedorFrete obterTipoProvedorFrete() {
+        return TipoProvedorFrete.JADLOG;
+    }
+}
---
+++ Interfaces_Exercicios/resumo_pedido/provedores/Sedex.java
@@ -0,0 +1,12 @@
+package provedores;
+
+public class Sedex implements ProvedorFrete {
+    public Frete calcularFrete(double peso, double valor) {
+        double valorFrete = (peso > 1000) ? valor * 0.10 : valor * 0.05;
+        return new Frete(valorFrete, TipoProvedorFrete.SEDEX);
+    }
+
+    public TipoProvedorFrete obterTipoProvedorFrete() {
+        return TipoProvedorFrete.SEDEX;
+    }
+}
