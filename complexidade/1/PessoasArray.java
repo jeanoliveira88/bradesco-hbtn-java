4bca302661257256abd2ae6793f23c9fe41362a7
jean.oliveira <jean.valid@gmail.com>
2025-07-28 18:08:51 -0300

    Implementação complexidade.

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -23,11 +23,10 @@ public class PessoasArray {
         System.out.println("Procurando pelo nome: " + "\"" + nome + "\"");
 
         for (int i = 0; i < nomes.length; i++) {
-            System.out.println("Passando pelo indice: " + i);
+            System.out.println("Passando pelo indice:" + i);
             if (nomes[i] == nome) {
                 System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
                 encontrado = true;
-                break;
             }
         }
         if (!encontrado) {
