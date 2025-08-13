fac5eac6889b95529cbc15e6a76719844752ca27
Jean Oliveira <jean.valid@gmail.com>
2025-08-13 17:10:38 -0300

    Implementação Streams

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        List<Produto> livros = new ArrayList<>();
+        for (Produto p : pedido.getProdutos()) {
+            if (p.getCategoria() == CategoriaProduto.LIVRO) {
+                livros.add(p);
+            }
+        }
+        return livros;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+                .filter(p -> p.getPreco() >= precoMinimo)
+                .toList();
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream()
+                        .anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+                .toList();
+    }
+
+}
---
+++ Streams_Collectors_Optional/pedidos_eletronico/Pedido.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+class Pedido {
+    private int codigo;
+    private List<Produto> produtos;
+    private Cliente cliente;
+
+    public Pedido(int codigo, List<Produto> produtos, Cliente cliente) {
+        this.codigo = codigo;
+        this.produtos = produtos;
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + codigo + "] " + cliente.getNome();
+    }
+}
