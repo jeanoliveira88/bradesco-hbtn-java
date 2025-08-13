5944712452e6b3c291f1b2db11452d2b7b6aa0b7
Jean Oliveira <jean.valid@gmail.com>
2025-08-13 17:35:28 -0300

    implementação Collectors

---
+++ 0x19/acima_40/Pessoa.java
@@ -0,0 +1,28 @@
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.2f", codigo, nome, cargo, idade, salario);
+    }
+}
