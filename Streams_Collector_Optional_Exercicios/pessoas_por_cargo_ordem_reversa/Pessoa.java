885077c3781ddc6a3b7b07d79f1ff41c05e3f569
Jean Oliveira <jean.valid@gmail.com>
2025-10-01 16:18:46 -0300

    0. Pessoas agrupadas por cargos em order reversa

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -0,0 +1,46 @@
+package pessoas_por_cargo_ordem_reversa;
+
+import java.util.Objects;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.2f", codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        return Integer.compare(this.codigo, outra.codigo);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return codigo == pessoa.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
